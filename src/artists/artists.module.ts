1d1782de3af26abd0f4860b7761e8e4ab58a0f2c
Anna Biruk <49333189+anna-biruk@users.noreply.github.com>
2024-03-19 16:48:10 +0100

    implemented requests for artist

--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -3,8 +3,11 @@ import { ArtistsService } from './artists.service';
 import { ArtistsController } from './artists.controller';
 import { AlbumsService } from 'src/albums/albums.service';
 import { TracksService } from 'src/tracks/tracks.service';
+import { Artist } from './entities/artist.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Artist])],
   controllers: [ArtistsController],
   providers: [ArtistsService, AlbumsService, TracksService],
 })
--- src/artists/entities/artist.entity.ts
+++ src/artists/entities/artist.entity.ts
@@ -1,16 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 interface IArtist {
   id: string; // uuid v4
   name: string;
   grammy: boolean;
 }
+@Entity()
 export class Artist implements IArtist {
   constructor(id: string, name: string, grammy: boolean) {
     this.id = id;
     this.name = name;
     this.grammy = grammy;
   }
+  @PrimaryGeneratedColumn('uuid')
   id: string = uuid();
+  @Column()
   name: string;
+  @Column()
   grammy: boolean;
 }
